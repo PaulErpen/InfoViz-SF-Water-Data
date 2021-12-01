2efb7f86a2d32183484114f8fabcdb75088abbea
Paul Erpenstein <paulerpenstein@yahoo.de>
2021-12-01 15:14:03 +0100

    Updated the project to work with google app engine

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname  + '/index.html')
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 
 app.use("/", express.static(__dirname + "/public"))
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
